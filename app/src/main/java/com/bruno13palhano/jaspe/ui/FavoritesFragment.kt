699c469187df93a46f0624bb1fcbf86d00510a95
bruno13palhano <bruno13palhano@gmail.com>
2022-12-25 01:48:34 -0300

    first commit

---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/FavoritesFragment.kt
@@ -0,0 +1,19 @@
+package com.bruno13palhano.jaspe.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bruno13palhano.jaspe.R
+
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/HelpFragment.kt
@@ -0,0 +1,19 @@
+package com.bruno13palhano.jaspe.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bruno13palhano.jaspe.R
+
+class HelpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_help, container, false)
+    }
+
+}
