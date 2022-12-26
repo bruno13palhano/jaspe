11fb70bde8595b7d8728861c1fd219e1eadbceec
bruno13palhano <bruno13palhano@gmail.com>
2022-12-26 01:36:19 -0300

    Added new fragments

---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/AccountFragment.kt
@@ -0,0 +1,18 @@
+package com.bruno13palhano.jaspe.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bruno13palhano.jaspe.R
+
+class AccountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+}
