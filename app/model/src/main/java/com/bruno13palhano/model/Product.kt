b6afdf57bc4817769c5ab9b828cddfecbe7c15e6
bruno13palhano <bruno13palhano@gmail.com>
2022-12-29 20:09:26 -0300

    Added default value to prodcutId

--- app/model/src/main/java/com/bruno13palhano/model/Product.kt
+++ app/model/src/main/java/com/bruno13palhano/model/Product.kt
@@ -1,10 +1,11 @@
 package com.bruno13palhano.model
 
 data class Product(
-    val productId: Long,
+    val productId: Long = 0L,
     val productName: String,
     val productUrlImage: String = "",
     val productPrice: Float,
     val productType: String,
-    val productDescription: String = ""
+    val productDescription: String = "",
+    val productCompany: String,
 )
