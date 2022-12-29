71bef03533d6d9e5297255a808e50cb5a38be09a
bruno13palhano <bruno13palhano@gmail.com>
2022-12-29 00:29:50 -0300

    Util class

--- repository/src/main/java/com/bruno13palhano/repository/util/Utils.kt
+++ repository/src/main/java/com/bruno13palhano/repository/util/Utils.kt
@@ -1,2 +1,29 @@
 package com.bruno13palhano.repository.util
 
+import com.bruno13palhano.model.Product
+import com.bruno13palhano.repository.model.ProductRep
+
+internal fun convertProductRepToProduct(productRep: ProductRep): Product {
+    return Product(
+        productId = productRep.productId,
+        productName = productRep.productName,
+        productUrlImage = productRep.productUrlImage,
+        productPrice = productRep.productPrice,
+        productType = productRep.productType,
+        productDescription = productRep.productDescription
+    )
+}
+
+internal fun convertProductToProductRep(product: Product): ProductRep {
+    return ProductRep(
+        productId = product.productId,
+        productName = product.productName,
+        productUrlImage = product.productUrlImage,
+        productPrice = product.productPrice,
+        productType = product.productType,
+        productDescription = product.productDescription,
+        productCompany = ""
+    )
+}
+
+
