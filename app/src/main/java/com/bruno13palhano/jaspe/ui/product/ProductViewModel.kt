569d6d4c1be854d80560b702d02ba5ed6ba51cf7
bruno13palhano <bruno13palhano@gmail.com>
2022-12-29 22:18:40 -0300

    Added getProduct method

--- app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductViewModel.kt
@@ -1,7 +1,15 @@
 package com.bruno13palhano.jaspe.ui.product
 
 import androidx.lifecycle.ViewModel
+import com.bruno13palhano.model.Product
+import com.bruno13palhano.repository.ProductRepository
+import kotlinx.coroutines.flow.Flow
 
-class ProductViewModel : ViewModel() {
+class ProductViewModel(
+    private val productRepository: ProductRepository
+) : ViewModel() {
 
+    fun getProduct(productId: Long): Flow<Product> {
+        return productRepository.get(productId)
+    }
 }
