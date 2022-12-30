9bd385e290591bfdbd265f4c4596672582390bf7
bruno13palhano <bruno13palhano@gmail.com>
2022-12-30 19:45:41 -0300

    Favorite icon now change the color

--- app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductFragment.kt
@@ -1,6 +1,8 @@
 package com.bruno13palhano.jaspe.ui.product
 
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,11 +15,13 @@ import androidx.navigation.findNavController
 import com.bruno13palhano.jaspe.MainActivity
 import com.bruno13palhano.jaspe.R
 import com.bruno13palhano.jaspe.ui.ViewModelFactory
+import com.bruno13palhano.model.Product
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import kotlinx.coroutines.launch
 
 class ProductFragment : Fragment() {
+    private var productId: Long = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,17 +43,20 @@ class ProductFragment : Fragment() {
         val productPrice = view.findViewById<TextView>(R.id.product_price)
         val productType = view.findViewById<TextView>(R.id.product_type)
         val productDescription = view.findViewById<TextView>(R.id.product_description)
+        var url = "http://www.google.com"
 
         val buyButton = view.findViewById<ExtendedFloatingActionButton>(R.id.buy_product_button)
         buyButton.setOnClickListener {
-            println("buy button was clicked")
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
         }
 
-        val productId = ProductFragmentArgs.fromBundle(
+        productId = ProductFragmentArgs.fromBundle(
             requireArguments()
         ).productId
 
-        val viewModel = activity?.applicationContext?.let { ViewModelFactory(it, this@ProductFragment).createProductViewModel() }
+        val viewModel = activity?.applicationContext?.let {
+            ViewModelFactory(it, this@ProductFragment).createProductViewModel()
+        }
 
         lifecycle.coroutineScope.launch {
             viewModel?.getProduct(productId).let {
@@ -59,6 +66,7 @@ class ProductFragment : Fragment() {
                     productPrice.text = product.productPrice.toString()
                     productType.text = product.productType
                     productDescription.text = product.productDescription
+                    url = product.productUrlLink
                 }
             }
         }
@@ -71,10 +79,50 @@ class ProductFragment : Fragment() {
         toolbar.inflateMenu(R.menu.menu_toolbar_product)
         toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_ios_24)
 
+        val viewModel = activity?.applicationContext?.let {
+            ViewModelFactory(it, this@ProductFragment).createProductViewModel()
+        }
+
+        var favoriteProduct = Product(
+            productName = "",
+            productPrice = 0f,
+            productType = "",
+            productDescription = "",
+            productUrlImage = "",
+            productCompany = "",
+            productUrlLink = "",
+            productIsFavorite = false
+        )
+
+        lifecycle.coroutineScope.launch {
+            viewModel?.getProduct(productId)?.collect {
+                favoriteProduct = it
+                if (it.productIsFavorite) {
+                    toolbar.menu.getItem(0).icon?.setTint(resources.getColor(R.color.pink_light))
+                } else {
+                    toolbar.menu.getItem(0).icon?.setTint(resources.getColor(R.color.black))
+                }
+            }
+        }
+
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.favoriteProduct -> {
-                    println("favorite was clicked")
+                    lifecycle.coroutineScope.launch {
+                        val updatedProduct = Product(
+                            productId = favoriteProduct.productId,
+                            productName = favoriteProduct.productName,
+                            productUrlImage = favoriteProduct.productUrlImage,
+                            productType = favoriteProduct.productType,
+                            productPrice = favoriteProduct.productPrice,
+                            productDescription = favoriteProduct.productDescription,
+                            productCompany = favoriteProduct.productCompany,
+                            productUrlLink = favoriteProduct.productUrlLink,
+                            productIsFavorite = !favoriteProduct.productIsFavorite
+                        )
+
+                        viewModel?.updateProduct(updatedProduct)
+                    }
                     true
                 }
                 R.id.shareProduct -> {
