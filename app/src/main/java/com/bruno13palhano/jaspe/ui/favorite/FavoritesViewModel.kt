38dc3795ba0466637be08212e0e22888807d77c5
bruno13palhano <bruno13palhano@gmail.com>
2022-12-31 12:30:41 -0300

    Added getFavorites method

--- app/src/main/java/com/bruno13palhano/jaspe/ui/favorite/FavoritesViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/favorite/FavoritesViewModel.kt
@@ -1,10 +1,14 @@
 package com.bruno13palhano.jaspe.ui.favorite
 
 import androidx.lifecycle.ViewModel
+import com.bruno13palhano.model.Product
 import com.bruno13palhano.repository.ProductRepository
+import kotlinx.coroutines.flow.Flow
 
 class FavoritesViewModel(
     private val productRepository: ProductRepository
 ) : ViewModel(){
-
+    fun getAllFavorites(): Flow<List<Product>> {
+        return productRepository.getAllFavorites()
+    }
 }
