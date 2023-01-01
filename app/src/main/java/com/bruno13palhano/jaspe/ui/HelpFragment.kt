aa8346487a3874db2e686ab646f46fbe327fd20a
bruno13palhano <bruno13palhano@gmail.com>
2023-01-01 20:19:08 -0300

    Dispolay toolbar

--- app/src/main/java/com/bruno13palhano/jaspe/ui/HelpFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/HelpFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bruno13palhano.jaspe.MainActivity
 import com.bruno13palhano.jaspe.R
 
 class HelpFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).supportActionBar?.show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +22,8 @@ class HelpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_help, container, false)
     }
 
+    override fun onResume() {
+        super.onResume()
+        (activity as MainActivity).supportActionBar?.show()
+    }
 }
--- app/src/main/java/com/bruno13palhano/jaspe/ui/OffersFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/OffersFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bruno13palhano.jaspe.MainActivity
 import com.bruno13palhano.jaspe.R
 
 class OffersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).supportActionBar?.show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_offers, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as MainActivity).supportActionBar?.show()
     }
 }
