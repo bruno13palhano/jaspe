8eed1a95c0af945470fb532ef96622d14579b35d
bruno13palhano <bruno13palhano@gmail.com>
2023-01-03 18:38:31 -0300

    Removed main toolbar references

--- app/src/main/java/com/bruno13palhano/jaspe/ui/SearchFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/SearchFragment.kt
@@ -5,25 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bruno13palhano.jaspe.MainActivity
 import com.bruno13palhano.jaspe.R
 
 class SearchFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        (activity as MainActivity).supportActionBar?.show()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        (activity as MainActivity).supportActionBar?.show()
     }
 }
