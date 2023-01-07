ff8e57b6c2357663481ef68b66f4b7bd6e779be2
bruno13palhano <bruno13palhano@gmail.com>
2023-01-06 22:57:34 -0300

    Added BannerDao reference to database

--- repository/src/main/java/com/bruno13palhano/repository/ProductDatabase.kt
+++ repository/src/main/java/com/bruno13palhano/repository/ProductDatabase.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.bruno13palhano.repository.dao.BannerDao
 import com.bruno13palhano.repository.dao.ProductDao
 import com.bruno13palhano.repository.model.ProductRep
 
 @Database(entities = [ProductRep::class], version = 1, exportSchema = false)
 internal abstract class ProductDatabase : RoomDatabase() {
     abstract val productDao: ProductDao
+    abstract val bannerDao: BannerDao
 
     companion object {
         @Volatile
