ec81f5916f87f59dc94e0eb490563a0a91cbcd32
bruno13palhano <bruno13palhano@gmail.com>
2023-01-07 11:51:51 -0300

    Added a reference to banner repository

--- app/src/main/java/com/bruno13palhano/jaspe/ui/home/HomeViewModelFactory.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/home/HomeViewModelFactory.kt
@@ -2,15 +2,17 @@ package com.bruno13palhano.jaspe.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.repository.BannerRepository
 import com.bruno13palhano.repository.ProductRepository
 
 class HomeViewModelFactory(
-    private val repository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val bannerRepository: BannerRepository
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            return HomeViewModel(repository) as T
+            return HomeViewModel(productRepository, bannerRepository) as T
         }
 
         throw IllegalArgumentException("Unknown ViewModel")
