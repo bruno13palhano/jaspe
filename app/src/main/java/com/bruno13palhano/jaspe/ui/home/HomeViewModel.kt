3e504ab96e3116bdd3eff5150a5a4e9c79cba746
bruno13palhano <bruno13palhano@gmail.com>
2023-01-07 12:05:19 -0300

    Added a reference to banner repository

--- app/src/main/java/com/bruno13palhano/jaspe/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/home/HomeViewModel.kt
@@ -1,14 +1,34 @@
 package com.bruno13palhano.jaspe.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.model.Banner
 import com.bruno13palhano.model.Product
+import com.bruno13palhano.repository.BannerRepository
 import com.bruno13palhano.repository.ProductRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val bannerRepository: BannerRepository
 ) : ViewModel() {
+    private val _amazonBanner = MutableStateFlow(emptyList<Banner>())
+    val amazonBanner: StateFlow<List<Banner>> = _amazonBanner
+
+    init {
+        viewModelScope.launch {
+            bannerRepository.getByCompany(
+                "Amazon",
+                0,
+                1
+            ).collect { banners ->
+                _amazonBanner.value = banners
+            }
+        }
+    }
 
     suspend fun insertProduct(product: Product) {
         productRepository.insertProduct(product)
