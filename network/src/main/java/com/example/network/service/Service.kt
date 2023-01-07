2968ca0f6d32084322dc01f4d3112712f6f15130
bruno13palhano <bruno13palhano@gmail.com>
2023-01-06 21:41:08 -0300

    Added banner's methods

--- network/src/main/java/com/example/network/service/Service.kt
+++ network/src/main/java/com/example/network/service/Service.kt
@@ -1,6 +1,7 @@
 package com.example.network.service
 
-import com.example.network.ProductNet
+import com.example.network.model.BannerNet
+import com.example.network.model.ProductNet
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -24,4 +25,21 @@ internal interface Service {
 
     @GET("products/avon")
     suspend fun getAvonProducts(): List<ProductNet>
+
+    @GET("banner")
+    suspend fun getBannerById(
+        @Query(value = "bannerId")bannerId: Long
+    ): BannerNet
+
+    @GET("banners")
+    suspend fun getBanners(): List<BannerNet>
+
+    @GET("banners/amazon")
+    suspend fun getAmazonBanners(): List<BannerNet>
+
+    @GET("banners/natura")
+    suspend fun getNaturaBanners(): List<BannerNet>
+
+    @GET("banners/avon")
+    suspend fun getAvonBanners(): List<BannerNet>
 }
