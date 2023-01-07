449a7b605e8ed67a96322bcaa4098fda785c0598
bruno13palhano <bruno13palhano@gmail.com>
2023-01-06 21:49:24 -0300

    Added method to create BannerNetwork

--- network/src/main/java/com/example/network/service/NetworkFactory.kt
+++ network/src/main/java/com/example/network/service/NetworkFactory.kt
@@ -1,8 +1,17 @@
 package com.example.network.service
 
+import com.example.network.service.banner.BannerNetwork
+import com.example.network.service.banner.BannerNetworkImpl
+import com.example.network.service.product.ProductNetwork
+import com.example.network.service.product.ProductNetworkImpl
+
 class NetworkFactory {
 
     fun createProductNetWork(): ProductNetwork {
         return ProductNetworkImpl()
+    }
+
+    fun createBannerNetWork(): BannerNetwork {
+        return BannerNetworkImpl()
     }
 }
