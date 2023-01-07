daf8f91e7246077310c3b5d1dbf26da8f46e0a66
bruno13palhano <bruno13palhano@gmail.com>
2023-01-06 22:12:32 -0300

    Implemented methods

--- network/src/main/java/com/example/network/service/banner/BannerNetwork.kt
+++ network/src/main/java/com/example/network/service/banner/BannerNetwork.kt
@@ -1,4 +1,12 @@
 package com.example.network.service.banner
 
-interface BannerNetwok {
+import com.bruno13palhano.model.Banner
+import kotlinx.coroutines.flow.Flow
+
+internal interface BannerNetwork {
+    suspend fun getBanners(): Flow<List<Banner>>
+    suspend fun getBannerById(bannerId: Long): Flow<Banner>
+    suspend fun getAmazonBanners(): Flow<List<Banner>>
+    suspend fun getNaturaBanners(): Flow<List<Banner>>
+    suspend fun getAvonBanners(): Flow<List<Banner>>
 }
--- network/src/main/java/com/example/network/service/banner/BannerNetworkImpl.kt
+++ network/src/main/java/com/example/network/service/banner/BannerNetworkImpl.kt
@@ -1,4 +1,67 @@
 package com.example.network.service.banner
 
-class BannerNetworkImpl {
+import com.bruno13palhano.model.Banner
+import com.example.network.service.ApiService
+import com.example.network.util.convertBannerNetToBanner
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+internal class BannerNetworkImpl : BannerNetwork {
+    override suspend fun getBanners(): Flow<List<Banner>> = flow {
+        try {
+            emit(ApiService.ProductApi.apiService.getBanners().map {
+                convertBannerNetToBanner(it)
+            })
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override suspend fun getBannerById(bannerId: Long): Flow<Banner> = flow {
+        try {
+            emit(
+                convertBannerNetToBanner(
+                    ApiService.ProductApi.apiService.getBannerById(bannerId)
+                )
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override suspend fun getAmazonBanners(): Flow<List<Banner>> = flow {
+        try {
+            emit(
+                ApiService.ProductApi.apiService.getAmazonBanners().map {
+                    convertBannerNetToBanner(it)
+                }
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override suspend fun getNaturaBanners(): Flow<List<Banner>> = flow {
+        try {
+            emit(
+                ApiService.ProductApi.apiService.getNaturaBanners().map {
+                    convertBannerNetToBanner(it)
+                }
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override suspend fun getAvonBanners(): Flow<List<Banner>> = flow {
+        try {
+            emit(
+                ApiService.ProductApi.apiService.getAvonBanners().map {
+                    convertBannerNetToBanner(it)
+                }
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
