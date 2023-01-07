35f1291af85818ea30868c7abf634c04a494e5d6
bruno13palhano <bruno13palhano@gmail.com>
2023-01-06 23:43:22 -0300

    Internal modifier removed

--- network/src/main/java/com/example/network/service/banner/BannerNetwork.kt
+++ network/src/main/java/com/example/network/service/banner/BannerNetwork.kt
@@ -3,7 +3,7 @@ package com.example.network.service.banner
 import com.bruno13palhano.model.Banner
 import kotlinx.coroutines.flow.Flow
 
-internal interface BannerNetwork {
+interface BannerNetwork {
     suspend fun getBanners(): Flow<List<Banner>>
     suspend fun getBannerById(bannerId: Long): Flow<Banner>
     suspend fun getAmazonBanners(): Flow<List<Banner>>
