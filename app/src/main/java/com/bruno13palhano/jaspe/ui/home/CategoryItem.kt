a30eb2f6e998884b053f241ab601e4dbeaa11457
bruno13palhano <bruno13palhano@gmail.com>
2023-01-08 18:58:28 -0300

    Added category menu

---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/home/CategoryItem.kt
@@ -0,0 +1,8 @@
+package com.bruno13palhano.jaspe.ui.home
+
+data class CategoryItem(
+    val categoryTagName: String,
+    val categoryRouteName: String,
+    val categoryItemTitle: String,
+    val categoryItemImage: Int
+)
---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/home/CategoryItemAdapter.kt
@@ -0,0 +1,66 @@
+package com.bruno13palhano.jaspe.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bruno13palhano.jaspe.R
+
+class CategoryItemAdapter(
+    private val onClick: (String) -> Unit
+) : ListAdapter<CategoryItem, CategoryItemAdapter.CategoryItemViewHolder> (
+        CategoryItemDiffCallback()
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.category_card_item, parent, false) as LinearLayout
+        return CategoryItemViewHolder(view, onClick)
+    }
+
+    override fun onBindViewHolder(holder: CategoryItemViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    class CategoryItemViewHolder(
+        rootView: LinearLayout,
+        val onClick: (String) -> Unit
+    ) : RecyclerView.ViewHolder(rootView) {
+
+        private val categoryItemImage = rootView.findViewById<ImageView>(R.id.category_item_image)
+        private val categoryItemTitle = rootView.findViewById<TextView>(R.id.category_item_title)
+
+        var currentCategoryItem: CategoryItem? = null
+
+        init {
+            rootView.setOnClickListener {
+                currentCategoryItem?.let {
+                    onClick(it.categoryRouteName)
+                }
+            }
+        }
+
+        fun bind(item: CategoryItem) {
+            currentCategoryItem = item
+            categoryItemImage.setImageResource(item.categoryItemImage)
+            categoryItemTitle.text = item.categoryItemTitle
+        }
+    }
+
+    private class CategoryItemDiffCallback : DiffUtil.ItemCallback<CategoryItem>() {
+        override fun areItemsTheSame(oldItem: CategoryItem, newItem: CategoryItem): Boolean {
+            return oldItem.categoryTagName == newItem.categoryTagName
+        }
+
+        override fun areContentsTheSame(oldItem: CategoryItem, newItem: CategoryItem): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+}
