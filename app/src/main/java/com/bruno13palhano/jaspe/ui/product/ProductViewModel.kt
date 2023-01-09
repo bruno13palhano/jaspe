f018f2c3e60f37ae7a72c629f096ba4bedd4e914
bruno13palhano <bruno13palhano@gmail.com>
2023-01-09 15:10:42 -0300

    Added favortie product

--- app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductViewModel.kt
@@ -1,12 +1,15 @@
 package com.bruno13palhano.jaspe.ui.product
 
 import androidx.lifecycle.ViewModel
+import com.bruno13palhano.model.FavoriteProduct
 import com.bruno13palhano.model.Product
+import com.bruno13palhano.repository.FavoriteProductRepository
 import com.bruno13palhano.repository.ProductRepository
 import kotlinx.coroutines.flow.Flow
 
 class ProductViewModel(
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val favoriteProductRepository: FavoriteProductRepository
 ) : ViewModel() {
 
     fun getProduct(productId: Long): Flow<Product> {
@@ -15,5 +18,13 @@ class ProductViewModel(
 
     suspend fun updateProduct(product: Product) {
         productRepository.updateProduct(product)
+    }
+
+    fun getFavoriteProduct(favoriteProductId: Long): Flow<FavoriteProduct> {
+        return favoriteProductRepository.getFavoriteProduct(favoriteProductId)
+    }
+
+    suspend fun removeFavoriteProduct(favoriteProductId: Long) {
+        favoriteProductRepository.deleteFavoriteProductById(favoriteProductId)
     }
 }
--- app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductViewModelFactory.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/product/ProductViewModelFactory.kt
@@ -2,15 +2,17 @@ package com.bruno13palhano.jaspe.ui.product
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.repository.FavoriteProductRepository
 import com.bruno13palhano.repository.ProductRepository
 
 class ProductViewModelFactory(
-    private val repository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val favoriteProductRepository: FavoriteProductRepository
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
-            return ProductViewModel(repository) as T
+            return ProductViewModel(productRepository, favoriteProductRepository) as T
         }
 
         throw IllegalArgumentException("Unknown ViewModel")
