69a2316f7ee29a9cad80d72e9ac90b77c60a0e7a
bruno13palhano <bruno13palhano@gmail.com>
2023-01-10 18:49:00 -0300

    Added data from viewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/amazon/AmazonCategoryViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/amazon/AmazonCategoryViewModel.kt
@@ -1,4 +1,15 @@
 package com.bruno13palhano.jaspe.ui.category.amazon
 
-class AmazonCategoryViewModel {
+import androidx.lifecycle.ViewModel
+import com.bruno13palhano.model.Product
+import com.bruno13palhano.repository.ProductRepository
+import kotlinx.coroutines.flow.Flow
+
+class AmazonCategoryViewModel(
+    private val productRepository: ProductRepository
+) : ViewModel() {
+
+    fun getAllProducts(): Flow<List<Product>> {
+        return productRepository.getByCompany("Amazon", 0, 10)
+    }
 }
