8ad36a23b7bacf2ce5692a932116cb597d61cd97
bruno13palhano <bruno13palhano@gmail.com>
2023-01-10 19:31:38 -0300

    Added adapter to categories RecyclerViews

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoriesItemAdapter.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoriesItemAdapter.kt
@@ -1,4 +1,73 @@
 package com.bruno13palhano.jaspe.ui.category
 
-class CategoriesItemAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.model.Product
+
+class CategoriesItemAdapter(
+    private val onClick: (Long) -> Unit
+) : ListAdapter<Product, CategoriesItemAdapter.CategoriesItemViewHolder>(CategoriesDiffCallback()){
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoriesItemViewHolder{
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.categories_card_item, parent, false) as CardView
+        return CategoriesItemViewHolder(view, onClick)
+    }
+
+    override fun onBindViewHolder(
+        holder: CategoriesItemViewHolder,
+        position: Int
+    ) {
+        holder.bind(currentList[position])
+    }
+
+    class CategoriesItemViewHolder(
+        rootView: CardView,
+        val onClick: (Long) -> Unit
+    ) : RecyclerView.ViewHolder(rootView) {
+
+        private val productImage: ImageView = rootView.findViewById(R.id.product_image)
+        private val productName: TextView = rootView.findViewById(R.id.product_name)
+        private val productType: TextView = rootView.findViewById(R.id.product_type)
+        private val productPrice: TextView = rootView.findViewById(R.id.product_price)
+
+        var currentProduct: Product? = null
+
+        init {
+            rootView.setOnClickListener {
+                currentProduct?.let {
+                    onClick(it.productId)
+                }
+            }
+        }
+
+        fun bind(item: Product) {
+            currentProduct = item
+            productImage.load(item.productUrlImage)
+            productName.text = item.productName
+            productType.text = item.productType
+            productPrice.text = itemView.resources.getString(R.string.product_price_label, item.productPrice)
+        }
+    }
+
+    private class CategoriesDiffCallback : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.productId == newItem.productId
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
