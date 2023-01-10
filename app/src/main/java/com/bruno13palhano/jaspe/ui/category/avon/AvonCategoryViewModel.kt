84dc9c961bb0e16ad69223806cf368e2a0f6fbfe
bruno13palhano <bruno13palhano@gmail.com>
2023-01-10 19:23:44 -0300

    Added ViewModel

---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/avon/AvonCategoryViewModel.kt
@@ -0,0 +1,10 @@
+package com.bruno13palhano.jaspe.ui.category.avon
+
+import androidx.lifecycle.ViewModel
+import com.bruno13palhano.repository.ProductRepository
+
+class AvonCategoryViewModel(
+    private val productRepository: ProductRepository
+) : ViewModel(){
+
+}
