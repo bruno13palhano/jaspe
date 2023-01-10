3b2d0dd7afe33a4531d61812262e241270bbf87b
bruno13palhano <bruno13palhano@gmail.com>
2023-01-10 19:50:52 -0300

    Added ViewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/avon/AvonCategoryViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/avon/AvonCategoryViewModel.kt
@@ -1,10 +1,15 @@
 package com.bruno13palhano.jaspe.ui.category.avon
 
 import androidx.lifecycle.ViewModel
+import com.bruno13palhano.model.Product
 import com.bruno13palhano.repository.ProductRepository
+import kotlinx.coroutines.flow.Flow
 
 class AvonCategoryViewModel(
     private val productRepository: ProductRepository
 ) : ViewModel(){
 
+    fun getAllProducts(): Flow<List<Product>> {
+        return productRepository.getByCompany("Avon", 0, 10)
+    }
 }
--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/avon/AvonCategoryViewModelFactory.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/avon/AvonCategoryViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.bruno13palhano.jaspe.ui.category.avon
 
-class AvonCategoryViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.repository.ProductRepository
+
+class AvonCategoryViewModelFactory(
+    private val productRepository: ProductRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AvonCategoryViewModel::class.java)) {
+            return AvonCategoryViewModel(productRepository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel")
+    }
 }
