94dca471f12d43b64c1d4ebfe00dbb536da6e624
bruno13palhano <bruno13palhano@gmail.com>
2023-01-10 18:48:29 -0300

    Added data from viewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/amazon/AmazonCategoryViewModelFactory.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/amazon/AmazonCategoryViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.bruno13palhano.jaspe.ui.category.amazon
 
-class AmazonCategoryViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.repository.ProductRepository
+
+class AmazonCategoryViewModelFactory(
+    private val repository: ProductRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AmazonCategoryViewModel::class.java)) {
+            return AmazonCategoryViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel")
+    }
 }
