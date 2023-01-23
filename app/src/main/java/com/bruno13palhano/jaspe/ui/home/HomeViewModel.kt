a1dd3afbd9bed71e09649b2fa5a622a0ae7a5eaa
bruno13palhano <bruno13palhano@gmail.com>
2023-01-23 18:52:59 -0300

    removed unused methods

--- app/src/main/java/com/bruno13palhano/jaspe/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/home/HomeViewModel.kt
@@ -96,35 +96,7 @@ class HomeViewModel(
         }
     }
 
-    suspend fun insertProduct(product: Product) {
-        productRepository.insertProduct(product)
-    }
-
-    suspend fun updateProduct(product: Product) {
-        productRepository.updateProduct(product)
-    }
-
-    fun getProduct(productId: Long): Flow<Product> {
-        return productRepository.get(productId)
-    }
-
     fun getAllProducts(): Flow<List<Product>> {
         return productRepository.getAll()
-    }
-
-    fun getAmazonProducts(): Flow<List<Product>> {
-        return productRepository.getByCompany("Amazon", 0, 6)
-    }
-
-    fun getNaturaProducts(): Flow<List<Product>> {
-        return productRepository.getByCompany("Natura", 0, 6)
-    }
-
-    fun getAvonProducts(): Flow<List<Product>> {
-        return productRepository.getByCompany("Avon", 0, 6)
-    }
-
-    suspend fun deleteProductById(productId: Long) {
-        productRepository.deleteProductById(productId)
     }
 }
