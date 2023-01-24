fc862d9b72df2708c5d88783830fe5a72ad44d60
bruno13palhano <bruno13palhano@gmail.com>
2023-01-23 23:36:05 -0300

    Unused methods removed

--- repository/src/main/java/com/bruno13palhano/repository/BannerRepository.kt
+++ repository/src/main/java/com/bruno13palhano/repository/BannerRepository.kt
@@ -4,12 +4,6 @@ import com.bruno13palhano.model.Banner
 import kotlinx.coroutines.flow.Flow
 
 interface BannerRepository {
-    suspend fun insertBanner(banner: Banner)
     suspend fun insertBanners(bannerList: List<Banner>)
-    suspend fun updateBanner(banner: Banner)
-    suspend fun deleteBanner(banner: Banner)
-    suspend fun deleteBannerById(bannerId: Long)
-    fun get(bannerId: Long): Flow<Banner>
-    fun getAll(): Flow<List<Banner>>
     fun getByCompany(bannerCompany: String, offset: Int, limit: Int): Flow<List<Banner>>
 }
--- repository/src/main/java/com/bruno13palhano/repository/BannerRepositoryImpl.kt
+++ repository/src/main/java/com/bruno13palhano/repository/BannerRepositoryImpl.kt
@@ -10,40 +10,11 @@ import kotlinx.coroutines.flow.map
 internal class BannerRepositoryImpl(
     private val dao: BannerDao
 ) : BannerRepository {
-    override suspend fun insertBanner(banner: Banner) {
-        dao.insert(banner.asBannerRep())
-    }
 
     override suspend fun insertBanners(bannerList: List<Banner>) {
         dao.insertAll(bannerList.map {
             it.asBannerRep()
         })
-    }
-
-    override suspend fun updateBanner(banner: Banner) {
-        dao.update(banner.asBannerRep())
-    }
-
-    override suspend fun deleteBanner(banner: Banner) {
-        dao.delete(banner.asBannerRep())
-    }
-
-    override suspend fun deleteBannerById(bannerId: Long) {
-        dao.deleteById(bannerId)
-    }
-
-    override fun get(bannerId: Long): Flow<Banner> {
-        return dao.getBannerById(bannerId).map {
-            it.asBanner()
-        }
-    }
-
-    override fun getAll(): Flow<List<Banner>> {
-        return dao.getAll().map {
-            it.map { bannerRep ->
-                bannerRep.asBanner()
-            }
-        }
     }
 
     override fun getByCompany(
