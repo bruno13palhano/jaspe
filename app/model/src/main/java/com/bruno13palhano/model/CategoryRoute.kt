e74c1017edbac0458400c2fd4b9fd9f22ba57fa6
bruno13palhano <bruno13palhano@gmail.com>
2023-01-24 01:30:18 -0300

    Added new class to hold category routes

---
+++ app/model/src/main/java/com/bruno13palhano/model/CategoryRoute.kt
@@ -0,0 +1,10 @@
+package com.bruno13palhano.model
+
+enum class CategoryRoute(val route: String) {
+    BABY("category_baby"),
+    MARKET("category_market"),
+    AVON("category_avon"),
+    NATURA("category_natura"),
+    OFFERS("category_offers"),
+    HIGHLIGHTS("category_highlights")
+}
