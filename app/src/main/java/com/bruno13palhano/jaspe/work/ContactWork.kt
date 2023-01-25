27271af1f1eb39bf2244aa89ad66b183154fa281
bruno13palhano <bruno13palhano@gmail.com>
2023-01-25 20:22:17 -0300

    Get ContactInfo from network and add to database

--- app/src/main/java/com/bruno13palhano/jaspe/work/ContactWork.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/work/ContactWork.kt
@@ -1,4 +1,25 @@
 package com.bruno13palhano.jaspe.work
 
-class ContactWork {
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.bruno13palhano.repository.RepositoryFactory
+import com.example.network.service.NetworkFactory
+
+class ContactWork(
+    context: Context,
+    params: WorkerParameters
+) : CoroutineWorker(context, params){
+
+    private val contactInfoRepository = RepositoryFactory(context)
+        .createContactInfoRepository()
+    private val contactInfoNetwork = NetworkFactory().createContactInfoNetwork()
+
+    override suspend fun doWork(): Result {
+        contactInfoNetwork.getContactInfo().collect {
+            contactInfoRepository.insertContactInfo(it)
+        }
+
+        return Result.success()
+    }
 }
