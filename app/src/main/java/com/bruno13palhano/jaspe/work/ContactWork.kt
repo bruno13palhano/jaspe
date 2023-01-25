71fdb9d7a27bd1dc25944cb179eb67b83335217d
bruno13palhano <bruno13palhano@gmail.com>
2023-01-25 20:15:32 -0300

    Added createContactInfoNetwork mehtod

---
+++ app/src/main/java/com/bruno13palhano/jaspe/work/ContactWork.kt
@@ -0,0 +1,4 @@
+package com.bruno13palhano.jaspe.work
+
+class ContactWork {
+}
--- network/src/main/java/com/example/network/service/NetworkFactory.kt
+++ network/src/main/java/com/example/network/service/NetworkFactory.kt
@@ -2,16 +2,22 @@ package com.example.network.service
 
 import com.example.network.service.banner.BannerNetwork
 import com.example.network.service.banner.BannerNetworkImpl
+import com.example.network.service.contact.ContactInfoNetwork
+import com.example.network.service.contact.ContactInfoNetworkImp
 import com.example.network.service.product.ProductNetwork
 import com.example.network.service.product.ProductNetworkImpl
 
 class NetworkFactory {
 
-    fun createProductNetWork(): ProductNetwork {
+    fun createProductNetwork(): ProductNetwork {
         return ProductNetworkImpl()
     }
 
-    fun createBannerNetWork(): BannerNetwork {
+    fun createBannerNetwork(): BannerNetwork {
         return BannerNetworkImpl()
+    }
+
+    fun createContactInfoNetwork(): ContactInfoNetwork {
+        return ContactInfoNetworkImp()
     }
 }
