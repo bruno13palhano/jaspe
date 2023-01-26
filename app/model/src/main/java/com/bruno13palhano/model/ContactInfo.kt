5d1039400ded8179958f24fbd3d9dea5cb5e5f26
bruno13palhano <bruno13palhano@gmail.com>
2023-01-25 22:46:03 -0300

    Added defaul values

--- app/model/src/main/java/com/bruno13palhano/model/ContactInfo.kt
+++ app/model/src/main/java/com/bruno13palhano/model/ContactInfo.kt
@@ -1,7 +1,7 @@
 package com.bruno13palhano.model
 
 data class ContactInfo(
-    val contactWhatsApp: String,
-    val contactInstagram: String,
-    val contactEmail: String
+    val contactWhatsApp: String = "",
+    val contactInstagram: String = "",
+    val contactEmail: String = ""
 )
