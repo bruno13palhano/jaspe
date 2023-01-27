4fed8999e4216205e6ea27d16937ef3a5392519f
bruno13palhano <bruno13palhano@gmail.com>
2023-01-26 23:50:07 -0300

    Added highlight products

---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/home/ProductHorizontalItemAdapter.kt
@@ -0,0 +1,68 @@
+package com.bruno13palhano.jaspe.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.model.Product
+
+class ProductHorizontalItemAdapter(
+    private val onClick: (productUrlLink: String) -> Unit
+) : ListAdapter<Product, ProductHorizontalItemAdapter
+.ProductHorizontalItemViewHolder>(ProductHorizontalCallback()) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ProductHorizontalItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.product_card_horizontal, parent, false) as CardView
+        return ProductHorizontalItemViewHolder(view, onClick)
+    }
+
+    override fun onBindViewHolder(holder: ProductHorizontalItemViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    class ProductHorizontalItemViewHolder(
+        rootView: CardView,
+        val onClick: (productUrlLink: String) -> Unit
+    ) : RecyclerView.ViewHolder(rootView) {
+        private val productImage: ImageView = rootView.findViewById(R.id.product_image)
+        private val productDescription: TextView = rootView.findViewById(R.id.product_description)
+        private val productPrice: TextView = rootView.findViewById(R.id.product_price)
+
+        private var currentProduct: Product? = null
+
+        init {
+            rootView.setOnClickListener {
+                currentProduct?.let {
+                    onClick(it.productUrlLink)
+                }
+            }
+        }
+
+        fun bind(item: Product) {
+            currentProduct = item
+            productImage.load(item.productUrlImage)
+            productDescription.text = item.productDescription
+            productPrice.text = itemView.resources.getString(R.string.product_price_label, item.productPrice)
+        }
+    }
+
+    private class ProductHorizontalCallback : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.productUrlLink == newItem.productUrlLink
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
