d4a6693835a86fdc13b50e17c0f8ea3f75cba062
bruno13palhano <bruno13palhano@gmail.com>
2023-01-28 00:12:14 -0300

    Added productSeen to adapter onClick callback

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoriesItemAdapter.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoriesItemAdapter.kt
@@ -13,7 +13,7 @@ import com.bruno13palhano.jaspe.R
 import com.bruno13palhano.model.Product
 
 class CategoriesItemAdapter(
-    private val onClick: (productUrlLink: String) -> Unit
+    private val onClick: (productUrlLink: String, productSeen: Long) -> Unit
 ) : ListAdapter<Product, CategoriesItemAdapter.CategoriesItemViewHolder>(CategoriesDiffCallback()){
 
     override fun onCreateViewHolder(
@@ -34,7 +34,7 @@ class CategoriesItemAdapter(
 
     class CategoriesItemViewHolder(
         rootView: CardView,
-        val onClick: (productUrlLink: String) -> Unit
+        val onClick: (productUrlLink: String, productSeen: Long) -> Unit
     ) : RecyclerView.ViewHolder(rootView) {
 
         private val productImage: ImageView = rootView.findViewById(R.id.product_image)
@@ -47,7 +47,7 @@ class CategoriesItemAdapter(
         init {
             rootView.setOnClickListener {
                 currentProduct?.let {
-                    onClick(it.productUrlLink)
+                    onClick(it.productUrlLink, it.productSeen)
                 }
             }
         }
