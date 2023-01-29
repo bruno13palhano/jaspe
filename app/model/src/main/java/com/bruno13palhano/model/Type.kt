684bf0ab62c63816473ae284a80be4de8b46eec0
bruno13palhano <bruno13palhano@gmail.com>
2023-01-28 23:03:05 -0300

    Added new product Type model

---
+++ app/model/src/main/java/com/bruno13palhano/model/Type.kt
@@ -0,0 +1,9 @@
+package com.bruno13palhano.model
+
+enum class Type(val type: String) {
+    BABY("Infantil"),
+    MARKET("Mercado"),
+    MEN_PERFUME("Perfume masculino"),
+    WOMEN_PERFUME("Perfume feminino"),
+    BEAUTY_PRODUCTS("Productos de beleza")
+}
