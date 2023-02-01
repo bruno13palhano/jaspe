087494e50c25fa855e15f07b1170396aa717d313
bruno13palhano <bruno13palhano@gmail.com>
2023-01-31 22:54:32 -0300

    Added new routes

--- app/src/main/java/com/bruno13palhano/jaspe/ui/common/ProductUtil.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/common/ProductUtil.kt
@@ -7,6 +7,9 @@ import com.bruno13palhano.jaspe.ui.category.baby.BabyCategoryFragmentDirections
 import com.bruno13palhano.jaspe.ui.category.lastseen.LastSeenCategoryFragmentDirections
 import com.bruno13palhano.jaspe.ui.category.natura.NaturaCategoryFragmentDirections
 import com.bruno13palhano.jaspe.ui.category.offers.OffersCategoryFragmentDirections
+import com.bruno13palhano.jaspe.ui.home.HomeFragmentDirections
+import com.bruno13palhano.jaspe.ui.search.SearchDialogFragmentDirections
+import com.bruno13palhano.jaspe.ui.search.SearchFragmentDirections
 import com.bruno13palhano.model.Product
 import com.bruno13palhano.model.Route
 
@@ -52,6 +55,14 @@ fun navigateFromCategoryToProduct(
         Route.LAST_SEEN.route -> {
             navController.navigate(
                 LastSeenCategoryFragmentDirections.actionLastSeenCategoryToProduct(productUrlLink))
+        }
+        Route.SEARCH_DIALOG.route -> {
+            navController.navigate(
+                SearchDialogFragmentDirections.actionSearchDialogToSearch(productUrlLink))
+        }
+        Route.HOME.route -> {
+            navController.navigate(
+                HomeFragmentDirections.actionHomeToProduct(productUrlLink))
         }
     }
 }
