0d8c279e96592844497cd7c9fd68a15a3b19717e
bruno13palhano <bruno13palhano@gmail.com>
2023-02-03 16:13:13 -0300

    Added new FilterType

--- app/src/main/java/com/bruno13palhano/jaspe/ui/search/FilterType.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/search/FilterType.kt
@@ -1,6 +1,7 @@
 package com.bruno13palhano.jaspe.ui.search
 
 enum class FilterType(val filter: String) {
+    DEFAULT("default"),
     NAME("name"),
     LOW_PRICE("low_price"),
     HIGH_PRICE("high_price")
