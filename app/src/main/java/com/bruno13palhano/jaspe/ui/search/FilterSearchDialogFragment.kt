0976c5c06c82fbe7e755669dbbcf88a0ee03e1b4
bruno13palhano <bruno13palhano@gmail.com>
2023-02-03 14:35:46 -0300

    Added listener to positiveClick

--- app/src/main/java/com/bruno13palhano/jaspe/ui/search/FilterSearchDialogFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/search/FilterSearchDialogFragment.kt
@@ -1,4 +1,33 @@
 package com.bruno13palhano.jaspe.ui.search
 
-class FIlterSearchDialogFragment {
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.jaspe.ui.common.listOfFilter
+
+class FilterSearchDialogFragment(private val listener: FilterDialogListener) : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            var itemPosition = 0
+            val builder = AlertDialog.Builder(it, R.style.AlertDialogStyle)
+            builder.setTitle(R.string.filter_options_label)
+                .setSingleChoiceItems(R.array.array_filter_options, 0) { _, checkedItem ->
+                    itemPosition = checkedItem
+                }
+                .setPositiveButton(R.string.ok_button_label) { _, _ ->
+                    listener.onDialogPositiveClick(listOfFilter[itemPosition])
+                }
+                .setNegativeButton(R.string.cancel_button_label) { dialogInterface, _ ->
+                    dialogInterface.cancel()
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    interface FilterDialogListener {
+        fun onDialogPositiveClick(item: String)
+    }
 }
