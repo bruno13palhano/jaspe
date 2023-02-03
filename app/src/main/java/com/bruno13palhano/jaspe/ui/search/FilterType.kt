d142dbd574cb5580a10bc9f5b4a8209835aab8dd
bruno13palhano <bruno13palhano@gmail.com>
2023-02-03 16:06:07 -0300

    Added FilterType values

--- app/src/main/java/com/bruno13palhano/jaspe/ui/search/FilterType.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/search/FilterType.kt
@@ -1,4 +1,7 @@
 package com.bruno13palhano.jaspe.ui.search
 
-enum class FilterType {
+enum class FilterType(val filter: String) {
+    NAME("name"),
+    LOW_PRICE("low_price"),
+    HIGH_PRICE("high_price")
 }
