c6e9b70a13c8d8ef247dbc13349101f79e30b3e6
bruno13palhano <bruno13palhano@gmail.com>
2023-02-05 18:56:19 -0300

    Created common Categories route

---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/common/CategoriesViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.bruno13palhano.jaspe.ui.category.common
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.repository.external.ProductRepository
+
+class CategoriesVIewModelFactory(
+    private val productRepository: ProductRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CategoriesViewModel::class.java)) {
+            return CategoriesViewModel(productRepository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel")
+    }
+}
