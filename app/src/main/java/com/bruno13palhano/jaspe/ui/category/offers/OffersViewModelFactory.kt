7af7f8be0bb786da0db7d0b87797c7561ef03c9f
bruno13palhano <bruno13palhano@gmail.com>
2023-02-06 17:33:18 -0300

    OfferViewModelFactory

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/offers/OffersViewModelFactory.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/offers/OffersViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.bruno13palhano.jaspe.ui.category.offers
 
-class OffersViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.repository.external.ProductRepository
+
+class OffersViewModelFactory(
+    private val productRepository: ProductRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(OffersViewModel::class.java)) {
+            return OffersViewModel(productRepository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel")
+    }
 }
