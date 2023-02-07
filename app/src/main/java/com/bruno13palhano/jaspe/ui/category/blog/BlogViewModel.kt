ec05d9c3e2b405042d427ccb3dfd5872e3e597c9
bruno13palhano <bruno13palhano@gmail.com>
2023-02-07 15:17:19 -0300

    Implemented ViewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/blog/BlogViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/blog/BlogViewModel.kt
@@ -1,10 +1,24 @@
 package com.bruno13palhano.jaspe.ui.category.blog
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.model.BlogPost
 import com.bruno13palhano.repository.external.BlogPostRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class BlogPostViewModel(
+class BlogViewModel(
     private val blogPostRepository: BlogPostRepository
 ) : ViewModel() {
+    private val _allBlogPosts = MutableStateFlow<List<BlogPost>>(emptyList())
+    val allBlogPosts = _allBlogPosts.asStateFlow()
 
+    init {
+        viewModelScope.launch {
+            blogPostRepository.getAllBlogPosts().collect {
+                _allBlogPosts.value = it
+            }
+        }
+    }
 }
