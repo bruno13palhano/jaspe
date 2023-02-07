acf55007111d5fe98e2afb62078007e473a0cae5
bruno13palhano <bruno13palhano@gmail.com>
2023-02-07 17:33:04 -0300

    Added help ViewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/help/HelpViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/help/HelpViewModel.kt
@@ -1,4 +1,26 @@
 package com.bruno13palhano.jaspe.ui.help
 
-class HelpViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.model.ContactInfo
+import com.bruno13palhano.repository.external.ContactInfoRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class HelpViewModel(
+    private val contactInfoRepository: ContactInfoRepository
+) : ViewModel() {
+    private val _contactInfo = MutableStateFlow(ContactInfo())
+    val contactInfo = _contactInfo.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            try {
+                contactInfoRepository.getContactInfo(1L).collect {
+                    _contactInfo.value = it
+                }
+            } catch (ignored: Exception) {}
+        }
+    }
 }
--- app/src/main/java/com/bruno13palhano/jaspe/ui/help/HelpViewModelFactory.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/help/HelpViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.bruno13palhano.jaspe.ui.help
 
-class HelpViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.repository.external.ContactInfoRepository
+
+class HelpViewModelFactory(
+    private val contactInfoRepository: ContactInfoRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HelpViewModel::class.java)) {
+            return HelpViewModel(contactInfoRepository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel")
+    }
 }
