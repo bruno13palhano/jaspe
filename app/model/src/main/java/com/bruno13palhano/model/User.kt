9b1f1dc92d47057d3394c5b2a835844e93c7bb93
bruno13palhano <bruno13palhano@gmail.com>
2023-02-10 20:19:10 -0300

    Added User model

---
+++ app/model/src/main/java/com/bruno13palhano/model/User.kt
@@ -0,0 +1,9 @@
+package com.bruno13palhano.model
+
+data class User(
+    val uid: String = "",
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val urlPhoto: String = ""
+)
