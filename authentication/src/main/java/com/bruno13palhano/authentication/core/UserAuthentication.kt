b10380bbd44e5a2e01be9d175b53743cb2ffae71
bruno13palhano <bruno13palhano@gmail.com>
2023-02-10 21:00:37 -0300

    Added Authentication interface

--- authentication/src/main/java/com/bruno13palhano/authentication/core/UserAuthentication.kt
+++ authentication/src/main/java/com/bruno13palhano/authentication/core/UserAuthentication.kt
@@ -1,4 +1,13 @@
 package com.bruno13palhano.authentication.core
 
+import com.bruno13palhano.model.User
+
+
 interface UserAuthentication {
+    fun createUser(user: User, successfulCallback: () -> Unit, failedCallback: () -> Unit)
+    fun login(email: String, password: String, successfulCallback: () -> Unit, failedCallback: () -> Unit)
+    fun logout()
+    fun isUserAuthenticated(): Boolean
+    fun getCurrentUser(): User
+    fun updateUser(user: User)
 }
