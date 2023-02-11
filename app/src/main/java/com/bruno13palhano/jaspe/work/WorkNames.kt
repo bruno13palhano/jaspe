a81a04741cb8e880baf948583528c90a8e7435d9
bruno13palhano <bruno13palhano@gmail.com>
2023-02-11 18:47:16 -0300

    Created enum for work tags

---
+++ app/src/main/java/com/bruno13palhano/jaspe/work/WorkNames.kt
@@ -0,0 +1,9 @@
+package com.bruno13palhano.jaspe.work
+
+enum class WorkNames(val tag: String) {
+    PRODUCT("PRODUCT_WORK"),
+    BANNER("BANNER_WORK"),
+    CONTACT("CONTACT_WORK"),
+    BLOG("BLOG_WORK"),
+    NOTIFICATION("NOTIFICATION_WORK")
+}
