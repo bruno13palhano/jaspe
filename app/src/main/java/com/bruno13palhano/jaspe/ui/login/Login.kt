bb88c7a1ae12fd31e9a952d516a36a7927bdc7bb
bruno13palhano <bruno13palhano@gmail.com>
2023-02-13 23:10:58 -0300

    Added Login interface and refactored LoginViewModel

---
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/login/Login.kt
@@ -0,0 +1,5 @@
+package com.bruno13palhano.jaspe.ui.login
+
+internal interface Login {
+    fun login(email: String, password: String)
+}
--- app/src/main/java/com/bruno13palhano/jaspe/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/login/LoginViewModel.kt
@@ -2,22 +2,64 @@ package com.bruno13palhano.jaspe.ui.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.authentication.core.UserAuthentication
 import com.bruno13palhano.model.User
 import com.bruno13palhano.repository.external.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
-    private val userRepository: UserRepository
-) : ViewModel() {
+    private val userRepository: UserRepository,
+    private val authentication: UserAuthentication
+) : ViewModel(), Login {
+    private val _loginStatus = MutableStateFlow<LoginStatus>(LoginStatus.Loading)
+    val loginStatus = _loginStatus.asStateFlow()
 
-    fun insertUser(user: User) {
-        viewModelScope.launch {
-            userRepository.insertUser(user)
+    override fun login(email: String, password: String) {
+        if (isParamsValid(email, password)) {
+            authentication.login(
+                email = email,
+                password = password,
+                successfulCallback = {
+                    _loginStatus.value = LoginStatus.Success
+                    insertUser(authentication.getCurrentUser())
+                },
+                failedCallback = {
+                    _loginStatus.value = LoginStatus.Error
+                }
+            )
+        } else {
+            _loginStatus.value = LoginStatus.Error
         }
     }
 
     fun getUserByUid(userUid: String): Flow<User> {
         return userRepository.getUserByUid(userUid)
     }
+
+    fun isUserAuthenticated(): Boolean {
+        return if(authentication.isUserAuthenticated()) {
+            insertUser(authentication.getCurrentUser())
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun insertUser(user: User) {
+        viewModelScope.launch {
+            userRepository.insertUser(user)
+        }
+    }
+
+    private fun isParamsValid(email: String, password: String): Boolean =
+        email != "" && password != ""
+}
+
+sealed class LoginStatus {
+    object Loading: LoginStatus()
+    object Error : LoginStatus()
+    object Success : LoginStatus()
 }
