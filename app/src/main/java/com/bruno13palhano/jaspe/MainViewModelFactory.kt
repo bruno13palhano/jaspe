a34d07d8b673eec22feb21817c8aee792a3209bd
bruno13palhano <bruno13palhano@gmail.com>
2023-02-16 23:21:01 -0300

    Added a UserRepository and UserAuthentication references

--- app/src/main/java/com/bruno13palhano/jaspe/MainViewModelFactory.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/MainViewModelFactory.kt
@@ -3,16 +3,25 @@ package com.bruno13palhano.jaspe
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bruno13palhano.authentication.core.UserAuthentication
 import com.bruno13palhano.repository.external.ContactInfoRepository
+import com.bruno13palhano.repository.external.UserRepository
 
 class MainViewModelFactory(
     private val application: Application,
-    private val contactInfoRepository: ContactInfoRepository
+    private val contactInfoRepository: ContactInfoRepository,
+    private val userRepository: UserRepository,
+    private val authentication: UserAuthentication
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            return MainViewModel(application, contactInfoRepository) as T
+            return MainViewModel(
+                application = application,
+                contactInfoRepository = contactInfoRepository,
+                userRepository = userRepository,
+                authentication = authentication
+            ) as T
         }
 
         throw IllegalArgumentException("Unknown ViewModel")
