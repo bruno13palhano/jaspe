63b71da4452adf2fb00ed9084490c7a704bbf243
bruno13palhano <bruno13palhano@gmail.com>
2023-02-18 23:16:24 -0300

    Removed unused import

--- app/src/main/java/com/bruno13palhano/jaspe/ui/account/AccountFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/account/AccountFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Lifecycle
