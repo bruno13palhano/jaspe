d7a6efdf7f2c4c0d263544aa9e8add1ea5ef3914
bruno13palhano <bruno13palhano@gmail.com>
2023-02-20 00:33:16 -0300

    Added notification dao to repository

---
+++ repository/src/main/java/com/bruno13palhano/repository/dao/NotificationDao.kt
@@ -0,0 +1,23 @@
+package com.bruno13palhano.repository.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import com.bruno13palhano.model.Notification
+import com.bruno13palhano.repository.model.NotificationRep
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+internal interface NotificationDao {
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insert(notification: NotificationRep)
+
+    @Delete
+    suspend fun delete(notification: NotificationRep)
+
+    @Query("SELECT * FROM notification_table")
+    fun getAllNotifications(): Flow<List<Notification>>
+}
