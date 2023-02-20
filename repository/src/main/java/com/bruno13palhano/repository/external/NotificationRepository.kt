7f3db9cc6c45dd0bd7bf81039f665f6024341df6
bruno13palhano <bruno13palhano@gmail.com>
2023-02-20 00:39:31 -0300

    Added notification repository interface to repository

---
+++ repository/src/main/java/com/bruno13palhano/repository/external/NotificationRepository.kt
@@ -0,0 +1,10 @@
+package com.bruno13palhano.repository.external
+
+import com.bruno13palhano.model.Notification
+import kotlinx.coroutines.flow.Flow
+
+interface NotificationRepository {
+    suspend fun insertNotification(notification: Notification)
+    suspend fun deleteNotification(notification: Notification)
+    fun getAllNotifications(): Flow<List<Notification>>
+}
