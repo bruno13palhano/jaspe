923f518aff9b9368225a776e139ee56008a5934d
bruno13palhano <bruno13palhano@gmail.com>
2023-02-20 00:42:56 -0300

    Added a NotificationRepository implementation

---
+++ repository/src/main/java/com/bruno13palhano/repository/external/NotificationRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.bruno13palhano.repository.external
+
+import com.bruno13palhano.model.Notification
+import com.bruno13palhano.repository.dao.NotificationDao
+import com.bruno13palhano.repository.model.asNotification
+import com.bruno13palhano.repository.model.asNotificationRep
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+internal class NotificationRepositoryImpl(
+    private val notificationDao: NotificationDao
+) : NotificationRepository {
+
+    override suspend fun insertNotification(notification: Notification) {
+        notificationDao.insert(notification.asNotificationRep())
+    }
+
+    override suspend fun deleteNotification(notification: Notification) {
+        notificationDao.delete(notification.asNotificationRep())
+    }
+
+    override fun getAllNotifications(): Flow<List<Notification>> {
+        return notificationDao.getAllNotifications().map {
+            it.map { notificationRep ->
+                notificationRep.asNotification()
+            }
+        }
+    }
+}
