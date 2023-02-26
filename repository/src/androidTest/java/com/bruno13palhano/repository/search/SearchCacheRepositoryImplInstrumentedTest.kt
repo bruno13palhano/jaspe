63a33d9218b444da7a1ba42e246ae8093ebcced1
bruno13palhano <bruno13palhano@gmail.com>
2023-02-26 16:17:31 -0300

    Added instrumented test for SearchCacheRepositoryImpl

---
+++ repository/src/androidTest/java/com/bruno13palhano/repository/search/SearchCacheRepositoryImplInstrumentedTest.kt
@@ -0,0 +1,74 @@
+package com.bruno13palhano.repository.search
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import app.cash.turbine.test
+import com.bruno13palhano.repository.JaspeDatabase
+import com.bruno13palhano.repository.dao.SearchCacheRepDao
+import com.bruno13palhano.repository.external.SearchCacheRepository
+import com.bruno13palhano.repository.external.SearchCacheRepositoryImpl
+import com.bruno13palhano.repository.search.SearchCacheFactory.makeSearchCache
+import kotlinx.coroutines.runBlocking
+import org.junit.*
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class SearchCacheRepositoryImplInstrumentedTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var jaspeDatabase: JaspeDatabase
+    private lateinit var searchCacheDao: SearchCacheRepDao
+    private lateinit var repository: SearchCacheRepository
+
+    @Before
+    fun initDB() {
+        jaspeDatabase = Room.inMemoryDatabaseBuilder(
+            InstrumentationRegistry.getInstrumentation().context,
+            JaspeDatabase::class.java
+        ).build()
+        searchCacheDao = jaspeDatabase.searchCacheDao
+        repository = SearchCacheRepositoryImpl(searchCacheDao)
+    }
+
+    @After
+    fun closeDB() {
+        jaspeDatabase.close()
+    }
+
+    @Test
+    fun insertSearchCache_shouldPersistSearchCacheInDB() = runBlocking {
+        val searchCache = makeSearchCache()
+
+        repository.insertSearchCache(searchCache)
+
+        repository.getAllSearchCache().test {
+            val currentSearchesCache = awaitItem()
+
+            Assert.assertEquals(searchCache, currentSearchesCache[0])
+            cancel()
+        }
+    }
+
+    @Test
+    fun deleteSearchCache_shouldDeleteSearchCacheInDB() = runBlocking {
+        val firstSearchCache = makeSearchCache()
+        val secondSearchCache = makeSearchCache()
+
+        repository.insertSearchCache(firstSearchCache)
+        repository.insertSearchCache(secondSearchCache)
+
+        repository.deleteSearchCacheById(firstSearchCache.searchCacheId)
+
+        repository.getAllSearchCache().test {
+            val currentSearchesCache = awaitItem()
+
+            Assert.assertFalse(currentSearchesCache.contains(firstSearchCache))
+            cancel()
+        }
+    }
+}
