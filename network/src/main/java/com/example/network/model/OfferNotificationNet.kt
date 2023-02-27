73c9eaea24bd1d094f460f7e11abafe3544725ff
bruno13palhano <bruno13palhano@gmail.com>
2023-02-27 14:02:11 -0300

    Added Json annotation

--- network/src/main/java/com/example/network/model/OfferNotificationNet.kt
+++ network/src/main/java/com/example/network/model/OfferNotificationNet.kt
@@ -1,10 +1,16 @@
 package com.example.network.model
 
 import com.bruno13palhano.model.Notification
+import com.squareup.moshi.Json
 
 internal data class OfferNotificationNet(
+    @Json(name = "title")
     val title: String,
+
+    @Json(name = "description")
     val description: String,
+
+    @Json(name = "type")
     val type: String
 )
 
