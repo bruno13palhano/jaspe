5bd49946179402337d97ca9531c8fc3aa8c0190e
bruno13palhano <bruno13palhano@gmail.com>
2023-03-01 00:31:39 -0300

    Added a ModelFactory for test

---
+++ app/src/test/java/com/bruno13palhano/jaspe/ui/home/ModelFactory.kt
@@ -0,0 +1,67 @@
+package com.bruno13palhano.jaspe.ui.home
+
+import com.bruno13palhano.model.*
+import java.util.UUID
+import java.util.concurrent.ThreadLocalRandom
+
+object ModelFactory {
+
+    fun makeProduct() = Product(
+        productId = makeRandomLong(),
+        productName = makeRandomString(),
+        productUrlImage = makeRandomString(),
+        productPrice = makeRandomFloat(),
+        productDescription = makeRandomString(),
+        productCompany = makeRandomString(),
+        productType = makeRandomString(),
+        productUrlLink = makeRandomString()
+    )
+
+    fun makeProduct(company: String) = Product(
+        productId = makeRandomLong(),
+        productName = makeRandomString(),
+        productUrlImage = makeRandomString(),
+        productPrice = makeRandomFloat(),
+        productDescription = makeRandomString(),
+        productCompany = company,
+        productType = makeRandomString(),
+        productUrlLink = makeRandomString()
+    )
+
+    fun makeBanner(company: String) = Banner(
+        bannerId = makeRandomLong(),
+        bannerName = makeRandomString(),
+        bannerUrlImage = makeRandomString(),
+        bannerCompany = company
+    )
+
+    fun makeNotification(isVisualized: Boolean) = Notification(
+        id = makeRandomLong(),
+        title = makeRandomString(),
+        description = makeRandomString(),
+        isVisualized = isVisualized,
+        type = makeRandomString()
+    )
+
+    fun makeContactInfo() = ContactInfo(
+        contactWhatsApp = makeRandomString(),
+        contactInstagram = makeRandomString(),
+        contactEmail = makeRandomString()
+    )
+
+    fun makeUser() = User(
+        uid = makeRandomString(),
+        username = makeRandomString(),
+        email = makeRandomString(),
+        password = "",
+        urlPhoto = makeRandomString()
+    )
+
+    fun makeRandomString() = UUID.randomUUID().toString()
+
+    fun makeRandomLong() = ThreadLocalRandom.current()
+        .nextLong(0, 1000)
+
+    fun makeRandomFloat() = ThreadLocalRandom.current()
+        .nextFloat()
+}
