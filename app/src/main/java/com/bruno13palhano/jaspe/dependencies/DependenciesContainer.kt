73cb19542b342ceb1865121aa9eb504ff7174217
bruno13palhano <bruno13palhano@gmail.com>
2023-03-05 01:07:32 -0300

    Added a container to manager dependencies

---
+++ app/src/main/java/com/bruno13palhano/jaspe/dependencies/DependenciesContainer.kt
@@ -0,0 +1,27 @@
+package com.bruno13palhano.jaspe.dependencies
+
+import android.app.Application
+import com.bruno13palhano.jaspe.dependencies.factories.MainViewModelFactory
+import com.bruno13palhano.jaspe.dependencies.factories.ProductViewModelFactory
+import com.bruno13palhano.repository.RepositoryFactory
+
+class DependenciesContainer(
+    application: Application,
+) {
+    private val contactInfoRepository = RepositoryFactory(application)
+        .createContactInfoRepository()
+
+    private val productRepository = RepositoryFactory(application)
+        .createProductRepository()
+
+    private val favoriteProductRepository = RepositoryFactory(application)
+        .createFavoriteProductRepository()
+
+    val mainViewModelFactory = MainViewModelFactory(application, contactInfoRepository)
+
+    val productViewModelFactory = ProductViewModelFactory(
+        productRepository = productRepository,
+        favoriteProductRepository = favoriteProductRepository,
+        contactInfoRepository = contactInfoRepository
+    )
+}
