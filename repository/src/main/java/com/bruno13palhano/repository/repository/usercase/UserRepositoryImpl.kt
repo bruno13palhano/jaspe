f21f4a5d6cc4b337139d6d37df668bb2aae63ab9
bruno13palhano <bruno13palhano@gmail.com>
2023-03-07 23:06:39 -0300

    Added Hilt dependency injection in UserRepositoryImpl

--- repository/src/main/java/com/bruno13palhano/repository/repository/usercase/UserRepositoryImpl.kt
+++ repository/src/main/java/com/bruno13palhano/repository/repository/usercase/UserRepositoryImpl.kt
@@ -1,13 +1,17 @@
-package com.bruno13palhano.repository.repository
+package com.bruno13palhano.repository.repository.usercase
 
 import com.bruno13palhano.model.User
 import com.bruno13palhano.repository.database.dao.UserDao
 import com.bruno13palhano.repository.model.asUser
 import com.bruno13palhano.repository.model.asUserRep
+import com.bruno13palhano.repository.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
 
-internal class UserRepositoryImpl(
+@Singleton
+internal class UserRepositoryImpl @Inject constructor(
     private val userDao: UserDao
 ) : UserRepository {
     override suspend fun insertUser(user: User) {
