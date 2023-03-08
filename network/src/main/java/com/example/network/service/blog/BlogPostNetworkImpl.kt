05a092a33c5e0b666ace66914ea978cb672c9ee5
bruno13palhano <bruno13palhano@gmail.com>
2023-03-08 19:06:40 -0300

    Added Hilt's dependecy injection to BlogPostNetworkImpl

--- network/src/main/java/com/example/network/service/blog/BlogPostNetworkImpl.kt
+++ network/src/main/java/com/example/network/service/blog/BlogPostNetworkImpl.kt
@@ -2,19 +2,19 @@ package com.example.network.service.blog
 
 import com.bruno13palhano.model.BlogPost
 import com.example.network.model.asBlogPost
-import com.example.network.service.ApiService
-import dagger.hilt.android.scopes.ActivityScoped
+import com.example.network.service.Service
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@ActivityScoped
+@Singleton
 internal class BlogPostNetworkImpl @Inject constructor(
-
+    private val apiService: Service
 ) : BlogPostNetwork {
     override suspend fun getBlogPosts(): Flow<List<BlogPost>> = flow {
         try {
-            emit(ApiService.ProductApi.apiService.getBlogPosts().map {
+            emit(apiService.getBlogPosts().map {
                 it.asBlogPost()
             })
         } catch (e: Exception) {
