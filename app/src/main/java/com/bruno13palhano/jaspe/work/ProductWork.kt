d622701beee6187101816023d4988d191a8d90d3
bruno13palhano <bruno13palhano@gmail.com>
2023-03-07 23:50:02 -0300

    Added Hilt dependency injection in ProductWork

--- app/src/main/java/com/bruno13palhano/jaspe/work/ProductWork.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/work/ProductWork.kt
@@ -1,17 +1,22 @@
 package com.bruno13palhano.jaspe.work
 
 import android.content.Context
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.bruno13palhano.repository.RepositoryFactory
+import com.bruno13palhano.repository.di.DefaultProductRepository
+import com.bruno13palhano.repository.repository.ProductRepository
 import com.example.network.service.NetworkFactory
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
-class ProductWork(
-    context: Context,
-    params: WorkerParameters
+@HiltWorker
+class ProductWork @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    @DefaultProductRepository val productRepository: ProductRepository
 ) : CoroutineWorker(context, params) {
 
-    private val productRepository = RepositoryFactory(context).createProductRepository()
     private val productNetwork = NetworkFactory().createProductNetwork()
 
     override suspend fun doWork(): Result {
