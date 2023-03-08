d5c6c74e8d78c94f3ab3edb097553c2722f2f12d
bruno13palhano <bruno13palhano@gmail.com>
2023-03-08 19:05:33 -0300

    Added Hilt's dependecy injection to ProductNetworkImpl

--- network/src/main/java/com/example/network/service/product/ProductNetworkImpl.kt
+++ network/src/main/java/com/example/network/service/product/ProductNetworkImpl.kt
@@ -2,20 +2,20 @@ package com.example.network.service.product
 
 import com.bruno13palhano.model.Product
 import com.example.network.model.asProduct
-import com.example.network.service.ApiService
-import dagger.hilt.android.scopes.ActivityScoped
+import com.example.network.service.Service
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@ActivityScoped
+@Singleton
 internal class ProductNetworkImpl @Inject constructor(
-
+    private val apiService: Service
 ) : ProductNetwork {
 
     override suspend fun getProducts(params: List<Int>): Flow<List<Product>> = flow {
         try {
-            emit(ApiService.ProductApi.apiService.getProducts(params).map {
+            emit(apiService.getProducts(params).map {
                 it.asProduct()
             })
         } catch (e: Exception) {
@@ -26,7 +26,7 @@ internal class ProductNetworkImpl @Inject constructor(
 
     override suspend fun getProductById(productId: Long): Flow<Product> = flow {
         try {
-            emit(ApiService.ProductApi.apiService.getProductById(productId).asProduct())
+            emit(apiService.getProductById(productId).asProduct())
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -35,7 +35,7 @@ internal class ProductNetworkImpl @Inject constructor(
     override suspend fun getAmazonProducts(): Flow<List<Product>> = flow {
         try {
             emit(
-                ApiService.ProductApi.apiService.getAmazonProducts().map {
+                apiService.getAmazonProducts().map {
                     it.asProduct()
                 }
             )
@@ -47,7 +47,7 @@ internal class ProductNetworkImpl @Inject constructor(
     override suspend fun getNaturaProducts(): Flow<List<Product>> = flow {
         try {
             emit(
-                ApiService.ProductApi.apiService.getNaturaProducts().map {
+                apiService.getNaturaProducts().map {
                     it.asProduct()
                 }
             )
@@ -59,7 +59,7 @@ internal class ProductNetworkImpl @Inject constructor(
     override suspend fun getAvonProducts(): Flow<List<Product>> = flow {
         try {
             emit(
-                ApiService.ProductApi.apiService.getAvonProducts().map {
+                apiService.getAvonProducts().map {
                     it.asProduct()
                 }
             )
