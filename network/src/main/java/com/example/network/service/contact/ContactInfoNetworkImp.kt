cd0df87986e9637b94ba36f52be1d9b080c4d374
bruno13palhano <bruno13palhano@gmail.com>
2023-03-08 19:06:23 -0300

    Added Hilt's dependecy injection to ContactInfoNetworkImpl

--- network/src/main/java/com/example/network/service/contact/ContactInfoNetworkImp.kt
+++ network/src/main/java/com/example/network/service/contact/ContactInfoNetworkImp.kt
@@ -2,19 +2,19 @@ package com.example.network.service.contact
 
 import com.bruno13palhano.model.ContactInfo
 import com.example.network.model.asContactInfo
-import com.example.network.service.ApiService
-import dagger.hilt.android.scopes.ActivityScoped
+import com.example.network.service.Service
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@ActivityScoped
+@Singleton
 internal class ContactInfoNetworkImp @Inject constructor(
-
+    private val apiService: Service
 ) : ContactInfoNetwork {
     override suspend fun getContactInfo(): Flow<ContactInfo>  = flow {
         try {
-            emit(ApiService.ProductApi.apiService.getContact().asContactInfo())
+            emit(apiService.getContact().asContactInfo())
         } catch (e: Exception) {
             e.printStackTrace()
         }
