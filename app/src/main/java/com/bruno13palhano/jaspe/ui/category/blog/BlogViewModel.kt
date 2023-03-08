b5ab9d97b96f7ba3445314b531062011d21ae7db
bruno13palhano <bruno13palhano@gmail.com>
2023-03-08 00:29:50 -0300

    Added Hilt dependency injection in BlogViewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/blog/BlogViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/blog/BlogViewModel.kt
@@ -3,12 +3,18 @@ package com.bruno13palhano.jaspe.ui.category.blog
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.model.BlogPost
-import com.bruno13palhano.repository.external.BlogPostRepository
+import com.bruno13palhano.repository.di.DefaultBannerRepository
+import com.bruno13palhano.repository.di.DefaultBlogPostRepository
+import com.bruno13palhano.repository.repository.BlogPostRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class BlogViewModel(
+@HiltViewModel
+class BlogViewModel @Inject constructor(
+    @DefaultBlogPostRepository
     private val blogPostRepository: BlogPostRepository
 ) : ViewModel() {
     private val _allBlogPosts = MutableStateFlow<List<BlogPost>>(emptyList())
