f5559992e15a8385ca9b887c743e569433a48bf7
bruno13palhano <bruno13palhano@gmail.com>
2023-03-07 23:38:21 -0300

    Added HiltWorkerFactory

--- app/src/main/java/com/bruno13palhano/jaspe/di/DependenciesApplication.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/di/DependenciesApplication.kt
@@ -1,12 +1,17 @@
-package com.bruno13palhano.jaspe.dependencies
+package com.bruno13palhano.jaspe.di
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
-class DependenciesApplication : Application() {
-    lateinit var dependenciesContainer: DependenciesContainer
+@HiltAndroidApp
+class DependenciesApplication : Application(), Configuration.Provider {
+    @Inject lateinit var workerFactory: HiltWorkerFactory
 
-    override fun onCreate() {
-        super.onCreate()
-        dependenciesContainer = DependenciesContainer(this)
-    }
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
