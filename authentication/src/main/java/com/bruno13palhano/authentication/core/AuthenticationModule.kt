54495c02e851abb6fdd63500ed6fc22e047e53f1
bruno13palhano <bruno13palhano@gmail.com>
2023-03-08 00:08:53 -0300

    Added UserFirebase bind

--- authentication/src/main/java/com/bruno13palhano/authentication/core/AuthenticationModule.kt
+++ authentication/src/main/java/com/bruno13palhano/authentication/core/AuthenticationModule.kt
@@ -7,14 +7,32 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+annotation class DefaultUserFirebase
 
 @InstallIn(SingletonComponent::class)
 @Module
-internal object AuthenticationProviderModule {
+internal abstract class AuthenticationModule {
+
+    @DefaultUserFirebase
+    @Singleton
+    @Binds
+    abstract fun bindDefaultUserFirebase(
+        authentication: UserFirebase
+    ): UserAuthentication
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal object AuthenticationProvidersModule {
 
     @Provides
     fun provideFirebaseAuth(): FirebaseAuth {
