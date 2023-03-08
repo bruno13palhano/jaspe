68e81de33e7e4eb20bc8f67cadb8de7085b6503f
bruno13palhano <bruno13palhano@gmail.com>
2023-03-07 23:05:34 -0300

    Added Hilt dependency injection in SearchCacheRepositoryImpl

--- repository/src/main/java/com/bruno13palhano/repository/repository/usercase/SearchCacheRepositoryImpl.kt
+++ repository/src/main/java/com/bruno13palhano/repository/repository/usercase/SearchCacheRepositoryImpl.kt
@@ -7,8 +7,11 @@ import com.bruno13palhano.repository.model.asSearchCacheRep
 import com.bruno13palhano.repository.repository.SearchCacheRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
 
-internal class SearchCacheRepositoryImpl(
+@Singleton
+internal class SearchCacheRepositoryImpl @Inject constructor(
     private val daoCache: SearchCacheDao
 ) : SearchCacheRepository {
     override suspend fun insertSearchCache(searchCache: SearchCache) {
