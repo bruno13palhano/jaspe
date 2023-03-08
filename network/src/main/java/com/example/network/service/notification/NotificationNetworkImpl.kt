98da380acef983ebcc8970a7b601d630cdffdf46
bruno13palhano <bruno13palhano@gmail.com>
2023-03-08 17:12:46 -0300

    Added Hilt dependency injection in NotificatitonNetworkImpl

--- network/src/main/java/com/example/network/service/notification/NotificationNetworkImpl.kt
+++ network/src/main/java/com/example/network/service/notification/NotificationNetworkImpl.kt
@@ -3,10 +3,15 @@ package com.example.network.service.notification
 import com.bruno13palhano.model.Notification
 import com.example.network.model.asOfferNotification
 import com.example.network.service.ApiService
+import dagger.hilt.android.scopes.ActivityScoped
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-internal class NotificationNetworkImpl : NotificationNetwork{
+@ActivityScoped
+internal class NotificationNetworkImpl @Inject constructor(
+
+) : NotificationNetwork{
     override suspend fun getOfferNotification(): Flow<Notification> = flow {
         try {
             emit(ApiService.ProductApi.apiService.getOfferNotification().asOfferNotification())
