8298ccdf7205a8b6f4a6f264d1f2015109883df6
bruno13palhano <bruno13palhano@gmail.com>
2023-03-07 23:43:28 -0300

    Added Hilt dependency injection in BannerWork

--- app/src/main/java/com/bruno13palhano/jaspe/work/BannerWork.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/work/BannerWork.kt
@@ -1,17 +1,22 @@
 package com.bruno13palhano.jaspe.work
 
 import android.content.Context
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.bruno13palhano.repository.RepositoryFactory
+import com.bruno13palhano.repository.di.DefaultBannerRepository
+import com.bruno13palhano.repository.repository.BannerRepository
 import com.example.network.service.NetworkFactory
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
-class BannerWork(
-    context: Context,
-    params: WorkerParameters
+@HiltWorker
+class BannerWork @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    @DefaultBannerRepository val bannerRepository: BannerRepository
 ) : CoroutineWorker(context, params) {
 
-    private val bannerRepository = RepositoryFactory(context).createBannerRepository()
     private val bannerNetwork = NetworkFactory().createBannerNetwork()
 
     override suspend fun doWork(): Result {
