ec0d759b7df2acad97eb410a04ba819911284169
bruno13palhano <bruno13palhano@gmail.com>
2023-03-08 00:31:38 -0300

    Added Hilt dependency injection in AccountViewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/account/AccountViewModel.kt
@@ -3,14 +3,22 @@ package com.bruno13palhano.jaspe.ui.account
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.authentication.core.DefaultUserFirebase
 import com.bruno13palhano.authentication.core.UserAuthentication
-import com.bruno13palhano.repository.external.UserRepository
+import com.bruno13palhano.repository.di.DefaultUserRepository
+import com.bruno13palhano.repository.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AccountViewModel(
+@HiltViewModel
+class AccountViewModel @Inject constructor(
+    @DefaultUserFirebase
     private val authentication: UserAuthentication,
+
+    @DefaultUserRepository
     private val userRepository: UserRepository
 ) : ViewModel() {
 
