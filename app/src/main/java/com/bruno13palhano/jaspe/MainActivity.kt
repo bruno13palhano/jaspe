8d123b32cad64ddb29ff9563fe90e546b5da8154
bruno13palhano <bruno13palhano@gmail.com>
2023-03-10 14:17:22 -0300

    Extracted the navigationTo method to simple state holder class

--- app/src/main/java/com/bruno13palhano/jaspe/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/MainActivity.kt
@@ -8,16 +8,11 @@ import androidx.activity.viewModels
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.coroutineScope
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
-import com.bruno13palhano.jaspe.ui.common.openEmail
-import com.bruno13palhano.jaspe.ui.common.openInstagram
-import com.bruno13palhano.jaspe.ui.common.openWhatsApp
 import com.bruno13palhano.model.ContactInfo
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
@@ -56,114 +51,14 @@ class MainActivity : AppCompatActivity(), DrawerLock {
         navView.setNavigationItemSelectedListener { item ->
             drawer.closeDrawer(GravityCompat.START)
 
-            navigateTo(
+            MainStateHolder.navigateTo(
                 navController = navController,
-                item = item
+                item = item,
+                context = this,
+                contactInfo = contactInfo
             )
 
             true
-        }
-    }
-
-    private fun navigateTo(
-        navController: NavController,
-        item: MenuItem
-    ) {
-        if (!item.isChecked) {
-            when (item.itemId) {
-                R.id.homeFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_home)
-                        }
-                    }
-                }
-
-                R.id.searchDialogFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_search_dialog)
-                        }
-                    }
-                }
-
-                R.id.offersFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_offers_category)
-                        }
-                    }
-                }
-
-                R.id.notificationsFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_notifications)
-                        }
-                    }
-                }
-
-                R.id.favoritesFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_favorites)
-                        }
-                    }
-                }
-
-                R.id.categoryFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_category)
-                        }
-                    }
-                }
-
-                R.id.accountFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_account)
-                        }
-                    }
-                }
-
-                R.id.blogFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_blog)
-                        }
-                    }
-                }
-
-                R.id.helpFragment -> {
-                    lifecycleScope.launch {
-                        navController.apply {
-                            popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
-                            navigate(R.id.action_to_help)
-                        }
-                    }
-                }
-
-                R.id.whatsapp -> {
-                    openWhatsApp(this, contactInfo.contactWhatsApp, "")
-                }
-
-                R.id.instagram -> {
-                    openInstagram(this, contactInfo.contactInstagram)
-                }
-
-                R.id.email -> {
-                    openEmail(this, contactInfo.contactEmail)
-                }
-            }
         }
     }
 
---
+++ app/src/main/java/com/bruno13palhano/jaspe/MainStateHolder.kt
@@ -0,0 +1,98 @@
+package com.bruno13palhano.jaspe
+
+import android.content.Context
+import android.view.MenuItem
+import androidx.navigation.NavController
+import com.bruno13palhano.jaspe.ui.common.openEmail
+import com.bruno13palhano.jaspe.ui.common.openInstagram
+import com.bruno13palhano.jaspe.ui.common.openWhatsApp
+import com.bruno13palhano.model.ContactInfo
+
+object MainStateHolder {
+
+    fun navigateTo(
+        navController: NavController,
+        item: MenuItem,
+        context: Context,
+        contactInfo: ContactInfo
+    ) {
+        if (!item.isChecked) {
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_home)
+                    }
+                }
+
+                R.id.searchDialogFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_search_dialog)
+                    }
+                }
+
+                R.id.offersFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_offers_category)
+                    }
+                }
+
+                R.id.notificationsFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_notifications)
+                    }
+                }
+
+                R.id.favoritesFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_favorites)
+                    }
+                }
+
+                R.id.categoryFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_category)
+                    }
+                }
+
+                R.id.accountFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_account)
+                    }
+                }
+
+                R.id.blogFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_blog)
+                    }
+                }
+
+                R.id.helpFragment -> {
+                    navController.apply {
+                        popBackStack(R.id.homeFragment, inclusive = false, saveState = true)
+                        navigate(R.id.action_to_help)
+                    }
+                }
+
+                R.id.whatsapp -> {
+                    openWhatsApp(context, contactInfo.contactWhatsApp, "")
+                }
+
+                R.id.instagram -> {
+                    openInstagram(context, contactInfo.contactInstagram)
+                }
+
+                R.id.email -> {
+                    openEmail(context, contactInfo.contactEmail)
+                }
+            }
+        }
+    }
+}
