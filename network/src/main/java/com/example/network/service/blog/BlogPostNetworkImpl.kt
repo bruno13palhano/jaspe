e17c1770c003c3e7aa9183aca98853e46712998f
bruno13palhano <bruno13palhano@gmail.com>
2023-03-12 13:45:17 -0300

    Added ShopDaniDispatchers dependency to BlogPostNetworkImpl

--- network/src/main/java/com/example/network/service/blog/BlogPostNetworkImpl.kt
+++ network/src/main/java/com/example/network/service/blog/BlogPostNetworkImpl.kt
@@ -1,9 +1,11 @@
 package com.example.network.service.blog
 
+import com.bruno13palhano.common.di.Dispatcher
+import com.bruno13palhano.common.di.ShopDaniDispatchers.IO
 import com.bruno13palhano.model.BlogPost
 import com.example.network.model.asBlogPost
 import com.example.network.service.Service
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
@@ -12,7 +14,8 @@ import javax.inject.Singleton
 
 @Singleton
 internal class BlogPostNetworkImpl @Inject constructor(
-    private val apiService: Service
+    private val apiService: Service,
+    @Dispatcher(IO) private val dispatcher: CoroutineDispatcher
 ) : BlogPostNetwork {
     override fun getBlogPosts(): Flow<List<BlogPost>> = flow {
         try {
@@ -22,5 +25,5 @@ internal class BlogPostNetworkImpl @Inject constructor(
         } catch (e: Exception) {
             e.printStackTrace()
         }
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(dispatcher)
 }
