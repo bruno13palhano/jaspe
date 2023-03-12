86ed24eb5222a79e5d92bcbbdf8d30b02449197e
bruno13palhano <bruno13palhano@gmail.com>
2023-03-12 13:46:13 -0300

    Added ShopDaniDispatchers dependency to NotificationNetworkImpl

--- network/src/main/java/com/example/network/service/notification/NotificationNetworkImpl.kt
+++ network/src/main/java/com/example/network/service/notification/NotificationNetworkImpl.kt
@@ -1,9 +1,11 @@
 package com.example.network.service.notification
 
+import com.bruno13palhano.common.di.Dispatcher
+import com.bruno13palhano.common.di.ShopDaniDispatchers.IO
 import com.bruno13palhano.model.Notification
 import com.example.network.model.asOfferNotification
 import com.example.network.service.Service
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
@@ -12,7 +14,8 @@ import javax.inject.Singleton
 
 @Singleton
 internal class NotificationNetworkImpl @Inject constructor(
-    private val apiService: Service
+    private val apiService: Service,
+    @Dispatcher(IO) private val dispatcher: CoroutineDispatcher
 ) : NotificationNetwork{
     override fun getOfferNotification(): Flow<Notification> = flow {
         try {
@@ -20,5 +23,5 @@ internal class NotificationNetworkImpl @Inject constructor(
         } catch (e: Exception) {
             e.printStackTrace()
         }
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(dispatcher)
 }
