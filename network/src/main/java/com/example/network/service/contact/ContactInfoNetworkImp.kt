4a2db119c1b7ef4c19afba58931d7f10c23c0518
bruno13palhano <bruno13palhano@gmail.com>
2023-03-12 13:45:40 -0300

    Added ShopDaniDispatchers dependency to ContactInfoNetworkImpl

--- network/src/main/java/com/example/network/service/contact/ContactInfoNetworkImp.kt
+++ network/src/main/java/com/example/network/service/contact/ContactInfoNetworkImp.kt
@@ -1,9 +1,11 @@
 package com.example.network.service.contact
 
+import com.bruno13palhano.common.di.Dispatcher
+import com.bruno13palhano.common.di.ShopDaniDispatchers.IO
 import com.bruno13palhano.model.ContactInfo
 import com.example.network.model.asContactInfo
 import com.example.network.service.Service
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
@@ -12,7 +14,8 @@ import javax.inject.Singleton
 
 @Singleton
 internal class ContactInfoNetworkImp @Inject constructor(
-    private val apiService: Service
+    private val apiService: Service,
+    @Dispatcher(IO) private val dispatcher: CoroutineDispatcher
 ) : ContactInfoNetwork {
     override fun getContactInfo(): Flow<ContactInfo>  = flow {
         try {
@@ -20,5 +23,5 @@ internal class ContactInfoNetworkImp @Inject constructor(
         } catch (e: Exception) {
             e.printStackTrace()
         }
-    }.flowOn(Dispatchers.IO)
+    }.flowOn(dispatcher)
 }
