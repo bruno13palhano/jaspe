0062cc03080f33339552143143fc1469f7b36980
bruno13palhano <bruno13palhano@gmail.com>
2023-03-17 18:56:51 -0300

    findViewById has been replaced by viewBinding

--- app/src/main/java/com/bruno13palhano/jaspe/ui/search/SearchDialogFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/search/SearchDialogFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -15,12 +14,11 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.jaspe.databinding.SearchDialogBinding
 import com.bruno13palhano.jaspe.ui.common.navigateToProduct
 import com.bruno13palhano.model.Route
 import com.bruno13palhano.model.SearchCache
-import com.google.android.material.appbar.MaterialToolbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -28,17 +26,19 @@ import kotlinx.coroutines.launch
 class SearchDialogFragment : DialogFragment() {
     private val viewModel: SearchDialogViewModel by viewModels()
     private lateinit var inputMethodManager: InputMethodManager
+    private var _binding: SearchDialogBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.search_dialog, container, false)
+        _binding = SearchDialogBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         inputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.search_list)
         val adapter = SearchCacheAdapterList(
             onItemClick = {
                 prepareNavigation(view, it)
@@ -47,7 +47,7 @@ class SearchDialogFragment : DialogFragment() {
                 viewModel.deleteSearchCacheById(it)
             }
         )
-        recyclerView.adapter = adapter
+        binding.searchList.adapter = adapter
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -62,14 +62,12 @@ class SearchDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar_search)
-        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+        binding.toolbarSearch.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
 
-        val searchText: AppCompatEditText = view.findViewById(R.id.search_text)
-        searchText.requestFocus()
-        inputMethodManager.showSoftInput(searchText, InputMethodManager.SHOW_IMPLICIT)
+        binding.searchText.requestFocus()
+        inputMethodManager.showSoftInput(binding.searchText, InputMethodManager.SHOW_IMPLICIT)
 
-        searchText.setOnEditorActionListener { textView, i, _ ->
+        binding.searchText.setOnEditorActionListener { textView, i, _ ->
             val textValue = textView.text.toString().trim()
 
             if (i == EditorInfo.IME_ACTION_SEARCH && isNotTextEmpty(textValue)) {
@@ -84,9 +82,14 @@ class SearchDialogFragment : DialogFragment() {
             false
         }
 
-        toolbar.setNavigationOnClickListener {
+        binding.toolbarSearch.setNavigationOnClickListener {
             view.findNavController().navigateUp()
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun isNotTextEmpty(text: String): Boolean {
