03e13ffd1b120ed79f6052a92c9362fcde424ec0
bruno13palhano <bruno13palhano@gmail.com>
2023-03-17 23:35:06 -0300

    findViewById has been replaced by viewBinding

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoryListAdapter.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoryListAdapter.kt
@@ -2,12 +2,10 @@ package com.bruno13palhano.jaspe.ui.category
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.jaspe.databinding.CategoryCardListBinding
 import com.bruno13palhano.model.CategoryItem
 
 class CategoryListAdapter(
@@ -15,9 +13,9 @@ class CategoryListAdapter(
 ) : ListAdapter<CategoryItem, CategoryListAdapter.CategoryListViewHolder>(CategoryDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryListViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.category_card_list, parent, false) as CardView
-        return CategoryListViewHolder(view, onCLick)
+        val binding = CategoryCardListBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryListViewHolder(binding, onCLick)
     }
 
     override fun onBindViewHolder(holder: CategoryListViewHolder, position: Int) {
@@ -25,14 +23,13 @@ class CategoryListAdapter(
     }
 
     class CategoryListViewHolder(
-        rootView: CardView,
+        val binding: CategoryCardListBinding,
         val onCLick: (route: String) -> Unit
-    ) : RecyclerView.ViewHolder(rootView) {
-        private val routeTitle: TextView = rootView.findViewById(R.id.category_title)
+    ) : RecyclerView.ViewHolder(binding.root) {
         var currentCategoryItem: CategoryItem? = null
 
         init {
-            rootView.setOnClickListener {
+            binding.root.setOnClickListener {
                 currentCategoryItem?.let {
                     onCLick(it.categoryItemRoute)
                 }
@@ -41,7 +38,7 @@ class CategoryListAdapter(
 
         fun bind(item: CategoryItem) {
             currentCategoryItem = item
-            routeTitle.text = item.categoryItemTitle
+            binding.categoryTitle.text = item.categoryItemTitle
         }
     }
 
