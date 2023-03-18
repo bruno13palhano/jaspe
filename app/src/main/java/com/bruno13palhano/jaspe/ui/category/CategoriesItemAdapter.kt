d3a1cbf823b3f59d6246efcde41afe2d6c0d4a52
bruno13palhano <bruno13palhano@gmail.com>
2023-03-17 23:39:44 -0300

    findViewById has been replaced by viewBinding

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoriesItemAdapter.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoriesItemAdapter.kt
@@ -2,50 +2,37 @@ package com.bruno13palhano.jaspe.ui.category
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.jaspe.databinding.CategoriesCardItemBinding
 import com.bruno13palhano.model.Product
 
 class CategoriesItemAdapter(
     private val onClick: (product: Product) -> Unit
 ) : ListAdapter<Product, CategoriesItemAdapter.CategoriesItemViewHolder>(CategoriesDiffCallback()){
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): CategoriesItemViewHolder{
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.categories_card_item, parent, false) as CardView
-        return CategoriesItemViewHolder(view, onClick)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesItemViewHolder{
+        val binding = CategoriesCardItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoriesItemViewHolder(binding, onClick)
     }
 
-    override fun onBindViewHolder(
-        holder: CategoriesItemViewHolder,
-        position: Int
-    ) {
+    override fun onBindViewHolder(holder: CategoriesItemViewHolder, position: Int) {
         holder.bind(currentList[position])
     }
 
     class CategoriesItemViewHolder(
-        rootView: CardView,
+        val binding: CategoriesCardItemBinding,
         val onClick: (product: Product) -> Unit
-    ) : RecyclerView.ViewHolder(rootView) {
-
-        private val productImage: ImageView = rootView.findViewById(R.id.product_image)
-        private val productName: TextView = rootView.findViewById(R.id.product_name)
-        private val productType: TextView = rootView.findViewById(R.id.product_type)
-        private val productPrice: TextView = rootView.findViewById(R.id.product_price)
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         var currentProduct: Product? = null
 
         init {
-            rootView.setOnClickListener {
+            binding.root.setOnClickListener {
                 currentProduct?.let {
                     onClick(it)
                 }
@@ -54,10 +41,10 @@ class CategoriesItemAdapter(
 
         fun bind(item: Product) {
             currentProduct = item
-            productImage.load(item.productUrlImage)
-            productName.text = item.productName
-            productType.text = item.productType
-            productPrice.text = itemView.resources.getString(R.string.product_price_label, item.productPrice)
+            binding.productImage.load(item.productUrlImage)
+            binding.productName.text = item.productName
+            binding.productType.text = item.productType
+            binding.productPrice.text = itemView.resources.getString(R.string.product_price_label, item.productPrice)
         }
     }
 
