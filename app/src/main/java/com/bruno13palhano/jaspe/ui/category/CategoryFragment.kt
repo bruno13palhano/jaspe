43c68a94d5a5606ebea2438448a4cb0782e3d0d1
bruno13palhano <bruno13palhano@gmail.com>
2023-03-17 21:04:48 -0300

    findViewById has been replaced by viewBinding

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/CategoryFragment.kt
@@ -6,25 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.jaspe.databinding.FragmentFavoritesBinding
 import com.bruno13palhano.jaspe.ui.common.categoryList
 import com.bruno13palhano.model.Route
-import com.google.android.material.appbar.MaterialToolbar
 
 class CategoryFragment : Fragment() {
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_category, container, false)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.category_list)
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         val adapter = CategoryListAdapter {
             navigateToCommonCategories(it)
         }
-        recyclerView.adapter = adapter
+        binding.favoriteList.adapter = adapter
         adapter.submitList(categoryList)
 
         return view
@@ -32,11 +33,10 @@ class CategoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar_category)
-        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
-        toolbar.title = getString(R.string.categories_label)
+        binding.toolbarFavorites.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+        binding.toolbarFavorites.title = getString(R.string.categories_label)
 
-        toolbar.setNavigationOnClickListener {
+        binding.toolbarFavorites.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
     }
