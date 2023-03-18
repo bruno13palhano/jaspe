c295e4ac54f15bf79ea7df6417fd9093e96ab015
bruno13palhano <bruno13palhano@gmail.com>
2023-03-18 00:08:34 -0300

    findViewById has been replaced by viewBinding

--- app/src/main/java/com/bruno13palhano/jaspe/ui/product/RelatedProductItemAdapter.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/product/RelatedProductItemAdapter.kt
@@ -2,25 +2,22 @@ package com.bruno13palhano.jaspe.ui.product
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.bruno13palhano.jaspe.R
+import com.bruno13palhano.jaspe.databinding.RelatedProductsCardItemBinding
 import com.bruno13palhano.model.Product
 
 class RelatedProductItemAdapter (
     private val onClick: (product: Product) -> Unit
 ) : ListAdapter<Product, RelatedProductItemAdapter.RelatedItemViewHolder>(RelatedDiffCallback()) {
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RelatedItemViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.related_products_card_item, parent, false) as CardView
-        return RelatedItemViewHolder(view, onClick)
+        val binding = RelatedProductsCardItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return RelatedItemViewHolder(binding, onClick)
     }
 
     override fun onBindViewHolder(holder: RelatedItemViewHolder, position: Int) {
@@ -28,17 +25,14 @@ class RelatedProductItemAdapter (
     }
 
     class RelatedItemViewHolder(
-        rootView: CardView,
+        val binding: RelatedProductsCardItemBinding,
         val onClick: (product: Product) -> Unit
-    ) : RecyclerView.ViewHolder(rootView) {
-        private val productName: TextView = rootView.findViewById(R.id.product_name)
-        private val productPrice: TextView = rootView.findViewById(R.id.product_price)
-        private val productImage: ImageView = rootView.findViewById(R.id.product_image)
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         private var currentProduct: Product? = null
 
         init {
-            rootView.setOnClickListener {
+            binding.root.setOnClickListener {
                 currentProduct?.let {
                     onClick(it)
                 }
@@ -47,9 +41,9 @@ class RelatedProductItemAdapter (
 
         fun bind(item: Product) {
             currentProduct = item
-            productName.text = item.productName
-            productPrice.text = itemView.resources.getString(R.string.product_price_label, item.productPrice)
-            productImage.load(item.productUrlImage)
+            binding.productName.text = item.productName
+            binding.productPrice.text = itemView.resources.getString(R.string.product_price_label, item.productPrice)
+            binding.productImage.load(item.productUrlImage)
         }
     }
 
