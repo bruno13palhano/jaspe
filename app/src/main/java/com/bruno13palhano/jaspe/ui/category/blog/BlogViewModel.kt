d835e52e19e5c9ed1671fa1cae21eac181d236bb
bruno13palhano <bruno13palhano@gmail.com>
2023-03-19 19:09:32 -0300

    Refactored BlogViewModel

--- app/src/main/java/com/bruno13palhano/jaspe/ui/category/blog/BlogViewModel.kt
+++ app/src/main/java/com/bruno13palhano/jaspe/ui/category/blog/BlogViewModel.kt
@@ -2,13 +2,11 @@ package com.bruno13palhano.jaspe.ui.category.blog
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bruno13palhano.model.BlogPost
 import com.bruno13palhano.repository.di.DefaultBlogPostRepository
 import com.bruno13palhano.repository.repository.BlogPostRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,14 +14,11 @@ class BlogViewModel @Inject constructor(
     @DefaultBlogPostRepository
     private val blogPostRepository: BlogPostRepository
 ) : ViewModel() {
-    private val _allBlogPosts = MutableStateFlow<List<BlogPost>>(emptyList())
-    val allBlogPosts = _allBlogPosts.asStateFlow()
 
-    init {
-        viewModelScope.launch {
-            blogPostRepository.getAllBlogPosts().collect {
-                _allBlogPosts.value = it
-            }
-        }
-    }
+    val allBlogPosts = blogPostRepository.getAllBlogPosts()
+        .stateIn(
+            initialValue = emptyList(),
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000)
+        )
 }
